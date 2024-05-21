af9de61ab0d6a5e96092599b53b91bd5b023edd9
jcsanguino10 <jc.sanguino10@uniandes.edu.co>
2024-05-20 19:06:06 -0500

    fix-changes

--- App/app.js
+++ App/app.js
@@ -1,15 +1,40 @@
 const express = require('express');
-const run_rules = require('run_rules')
+const run_rules = require('./rules_engine.js')
 const app = express();
 const port = 4000;
 
+app.use(express.json());
+
+const nools = require("nools");
+
+// Load the flow from the .nools file
+
+let flow = nools.compile("../Rules/rules.nools");
+
+let SistemaLogico = flow.getDefined("SistemaLogico");
+
+
 // POST request
 app.post('/question', (req, res) => {
-    const newQuestion = req.body; 
-    let res = run_rules(newQuestion.question)
-    res.status(201).json(res);
-});
+    // Create a session
+    let session = flow.getSession();
 
+    let sistemaLogico = new SistemaLogico();
+    sistemaLogico.estado =req.body.question;
+
+    // Insert the fact into the session
+    session.assert(sistemaLogico);
+    // Match the rules
+    session.match(function(err){
+        if(err){
+            console.error(err.stack);
+            return null
+        }else{
+            return res.status(201).json({options:sistemaLogico.opciones, message: sistemaLogico.mensaje});
+        }
+    })
+    session.dispose();
+});
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
